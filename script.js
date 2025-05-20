84ff18b87737efa839cdc8c10c4c193cc19b1fd3
aizhanzo <47106976+aizhanzo@users.noreply.github.com>
2025-05-20 17:43:49 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-<!-- script.js placeholder -->
+
+function checkQuiz(formId, correctAnswers) {
+  const form = document.getElementById(formId);
+  const inputs = form.querySelectorAll('input[type=radio]:checked');
+  let score = 0;
+  inputs.forEach((input, idx) => {
+    if (input.value === correctAnswers[idx]) score++;
+  });
+  form.nextElementSibling.innerText = `Дұрыс жауаптар саны: ${score} / ${correctAnswers.length}`;
+}
